56ea0b90ccd273c4f1f5bfb570aaef9f9355d525
ravi <parigha.nawale@gmail.com>
2023-01-14 14:55:32 +0530

    Chnages

--- src/main/java/com/devops/ServletInitializer.java
+++ src/main/java/com/devops/ServletInitializer.java
@@ -2,7 +2,9 @@ package com.devops;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ServletInitializer extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
