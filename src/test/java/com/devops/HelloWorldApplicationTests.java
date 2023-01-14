07ad344c15bf8885e325bebac9624fd74debcec6
ravi <parigha.nawale@gmail.com>
2023-01-14 09:46:48 +0530

    Initial Commit

---
+++ src/test/java/com/devops/HelloWorldApplicationTests.java
@@ -0,0 +1,40 @@
+package com.devops;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloWorldApplicationTests {
+
+	@Autowired
+    private MockMvc mockMvc;
+    
+    
+    @Test
+    @DisplayName("GET /hello")
+    void testGetProductByCode() throws Exception {
+        // Execute the GET request
+        mockMvc.perform(get("/hello"))
+        // Validate the response code and content type
+       .andExpect(status().isOk());
+    }
+    
+    static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
